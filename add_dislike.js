9db3aec7ea3be5371f6dad883fed1bf3c20d98a7
R4g3D <darryncull@gmail.com>
2015-07-22 13:52:56 +0200

    parse username

--- add_dislike.js
+++ add_dislike.js
@@ -1,7 +1,7 @@
 function insert_dislike_button(node){
 	for (i = 0; i < node.length; i++){
 	    var my_node = '<span><a class="UFIDislikeLink" href="#" role="button" aria-live="polite" title="Dislike this"><span>Dislike</span></a></span>';
-	    if (node[i].innerHTML.includes('UFILikeLink') && !node[i].innerHTML.includes('UFIDislikeLink')){
+	    if (node[i].innerHTML.includes('UFILikeLink') && !node[i].innerHTML.includes('UFIDislikeLink') && !node[i].innerHTML.includes('UFIDislikeCommentLink')){
 	    	node[i].innerHTML += my_node;
 	    }
 	}
@@ -9,8 +9,8 @@ function insert_dislike_button(node){
 
 function insert_dislike_button_comment(nodeCom){
 	for (j = 0; j < nodeCom.length; j++){
-	    var my_nodeCom = '<span> · </span><a class="UFIDislikeLink" href="#" role="button" title="Dislike this comment" >Dislike</a>';
-	    if (nodeCom[j].innerHTML.includes('UFILikeLink') && !nodeCom[j].innerHTML.includes('UFIDislikeLink')){
+	    var my_nodeCom = '<span> · </span><a class="UFIDislikeCommentLink" href="#" role="button" title="Dislike this comment" >Dislike</a>';
+	    if (nodeCom[j].innerHTML.includes('UFILikeLink') && !nodeCom[j].innerHTML.includes('UFIDislikeLink') && !nodeCom[j].innerHTML.includes('UFIDislikeCommentLink')){
 	    	nodeCom[j].innerHTML += my_nodeCom;
 	    }
 	}
@@ -18,7 +18,41 @@ function insert_dislike_button_comment(nodeCom){
 
 $('body').on('click', '.UFIDislikeLink', function(){
 	alert($(this).parents('.userContentWrapper').first().text());
+	var uname = $(this).parents('.userContentWrapper').first().find('._5pb8').first().attr('href');
+	uname = getUserName(uname);
+	alert(uname);
 });
+
+$('body').on('click', '.UFIDislikeCommentLink', function(){
+	alert($(this).parents('.userContentWrapper').first().text());
+	var uname = $(this).parents('.UFIRow').first().find('.UFICommentActorName').attr('href');
+	uname = getUserName(uname);
+	alert(uname);
+});
+
+function getUserName(uname){
+	uname = uname.replace('https://www.facebook.com/', '');
+	uname = uname.replace('https://facebook.com/', '');
+	var slash_index = uname.indexOf('/');
+    var question_index = uname.indexOf('?');
+    var uid = '';
+    if (slash_index != -1){
+        uname = uname.substring(0, slash_index);
+    } else if (question_index != -1) {
+    	uid = uname.substring(question_index, uname.length);
+        uname = uname.substring(0, question_index);
+        if (uid.includes('id=')){
+        	var ampersan_index = uid.indexOf('&');
+        	if (ampersan_index != -1){
+        		uid = uid.substring(0, uid.indexOf('&'));
+        	} else {
+        		uid = uid.substring(0, uid.length);
+        	}
+        	return uid;
+        }
+    }
+    return uname;
+}
 
 window.onload = function(){
     var node = document.getElementsByClassName('_4l5');
@@ -32,7 +66,6 @@ window.onload = function(){
 };
 
 window.setInterval(function(){
-    var mynode = document.getElementsByClassName('UFIDislikeLink');
     var node = document.getElementsByClassName('_4l5');
     var nodeCom = document.getElementsByClassName('fsm fwn fcg UFICommentActions');
     if ((node.length > 0)){
